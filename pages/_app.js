719875fbf377304d8a81d3ae97abc12682643af4
Mert Osanmaz <osanmaz@merts-mbp.home>
2021-10-19 16:45:51 +0100

    modulenotfound

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { wrapper } from 'lib/redux'
-import Layout from '../components/Layout/index'
+import Layout from '../components/Layout/index.js'
 import 'styles/globals.scss'
 
 const App = ({ Component, pageProps }) => <Layout Component={Component} pageProps={pageProps} />
